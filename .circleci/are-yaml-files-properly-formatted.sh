9513afccaf98480733d7cd56bcf6c0cde18175e1
Thomas Guillet <guillet.thomas@gmail.com>
2018-06-11 16:31:09 +0200

    fixup! Run YAML test file through round trip

--- .circleci/are-yaml-files-properly-formatted.sh
+++ .circleci/are-yaml-files-properly-formatted.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
-if git diff-index HEAD -- **/*.yaml
+shopt -s globstar
+
+if ! git diff-index --name-only --exit-code HEAD -- **/*.yaml
 then
-    echo "One YAML file or more is not appropriately formatted"
+    echo "This or those files are not appropriately formatted."
     echo "Run openfisca_france/scripts/yaml_round_trip.py to format files and git diff to see the differences."
     exit 3
 fi
--- .circleci/is-version-number-acceptable.sh
+++ .circleci/is-version-number-acceptable.sh
@@ -31,10 +31,3 @@ then
     echo "Look at the CONTRIBUTING.md file to learn how to write the changelog."
     exit 2
 fi
-
-if git diff-index HEAD -- **/*.yaml
-then
-    echo "One YAML file or more is not appropriately formatted"
-    echo "Run openfisca_france/scripts/yaml_round_trip.py to format files and git diff to see the differences."
-    exit 3
-fi
